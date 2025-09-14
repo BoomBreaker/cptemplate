fd534f2dae110a05650882eacc6f309e165acf27
BoomBreaker <107251597+BoomBreaker@users.noreply.github.com>
2025-09-14 13:08:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+i will update my progress here
+
+starting to learn basic techniques and DSA - 14 Sep 2025
+<img width="935" height="767" alt="image" src="https://github.com/user-attachments/assets/2962bd78-d398-4d16-a4d2-fa3229bb9af5" />
+
+
---
+++ template.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+
+// cheese nuts hello
+// stolen from Geothermal
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define F0R(i,a) for(int i=0;i<a;i++)
+#define trav(a,x) for (auto& a : x)
+#define pb push_back
+
+const char nl = '\n';
+
+void solve(){
+	//solution goes here
+}
+
+int main(){
+	//technically just optimization for I/O streams
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+
+	int t; 
+	cin >> t; 
+	while(t--)
+		solve();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
